41987f1868e514118c5ba3bcf047e0db73e45e0e
imoseyon <imoseyon@gmail.com>
2014-11-27 15:33:11 -0800

    interactiveX: change the name back to interactive for compatibility

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -149,7 +149,7 @@ static int cpufreq_governor_interactive(struct cpufreq_policy *policy,
 static
 #endif
 struct cpufreq_governor cpufreq_gov_interactive = {
-	.name = "interactiveX",
+	.name = "interactive",
 	.governor = cpufreq_governor_interactive,
 	.max_transition_latency = 10000000,
 	.owner = THIS_MODULE,
