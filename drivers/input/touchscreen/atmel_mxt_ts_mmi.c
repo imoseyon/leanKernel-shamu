935b4ec1e9f440748772115d86ddeb2909cf4082
flar2 <asegaert@gmail.com>
2014-11-29 02:16:52 -0500

    Wake gestures: sweep2wake, sweep2sleep and doubletap2wake
    Change-Id: I9070a015124a662743493adbe3948fd63d84dc03

--- drivers/input/touchscreen/atmel_mxt_ts_mmi.c
+++ drivers/input/touchscreen/atmel_mxt_ts_mmi.c
@@ -31,6 +31,10 @@
 #include <linux/semaphore.h>
 #include <linux/atomic.h>
 
+#ifdef CONFIG_WAKE_GESTURES
+#include <linux/wake_gestures.h>
+#endif
+
 enum {
 	STATE_UNKNOWN,
 	STATE_ACTIVE,
@@ -40,6 +44,9 @@ enum {
 	STATE_INIT,
 	STATE_FLASH,
 	STATE_QUERY,
+#ifdef CONFIG_WAKE_GESTURES
+	STATE_WG,
+#endif
 	STATE_INVALID
 };
 
@@ -2472,6 +2479,16 @@ static void mxt_set_sensor_state(struct mxt_data *data, int state)
 			mxt_sensor_state_config(data, SUSPEND_IDX);
 			break;
 
+#ifdef CONFIG_WAKE_GESTURES
+	case STATE_WG:
+		data->mode_is_wakeable = true;
+		data->enable_reporting = true;
+		mxt_enable_wakeup_source(data, true);
+		if (!data->in_bootloader)
+			mxt_sensor_state_config(data, ACTIVE_IDX);
+			break;
+#endif
+
 	case STATE_ACTIVE:
 		if (!data->in_bootloader)
 			mxt_sensor_state_config(data, ACTIVE_IDX);
@@ -2508,6 +2525,35 @@ static void mxt_set_sensor_state(struct mxt_data *data, int state)
 	if (state == STATE_ACTIVE)
 		mxt_irq_enable(data, true);
 }
+
+#ifdef CONFIG_WAKE_GESTURES
+struct mxt_data *gl_mxt_data;
+
+bool scr_suspended(void)
+{
+	struct mxt_data *mxt_data = gl_mxt_data;
+	return mxt_data->suspended;
+}
+
+void set_internal_dt(bool input)
+{
+	struct mxt_data *mxt_data = gl_mxt_data;
+
+	if (input) {
+		mxt_set_alternate_mode(mxt_data,
+			mxt_data->alternate_mode, true, true);
+	} else {
+		mxt_restore_default_mode(mxt_data);
+	}
+}
+
+bool get_internal_dt(void)
+{
+	struct mxt_data *mxt_data = gl_mxt_data;
+	return mxt_data->mode_is_wakeable;
+}
+#endif
+
 
 static void mxt_free_input_device(struct mxt_data *data)
 {
@@ -4890,6 +4936,10 @@ static int mxt_probe(struct i2c_client *client,
 	data->mem_access_created = true;
 	data->mode_is_persistent = true;
 
+#ifdef CONFIG_WAKE_GESTURES
+	gl_mxt_data = data;
+#endif
+
 	return 0;
 
 err_remove_sysfs_group:
@@ -4941,7 +4991,12 @@ static int mxt_suspend(struct device *dev)
 	if (atomic_cmpxchg(&data->suspended, 0, 1) == 0) {
 		mxt_lock(&data->crit_section_lock);
 
-		mxt_set_sensor_state(data, STATE_SUSPEND);
+#ifdef CONFIG_WAKE_GESTURES
+		if (s2w_switch)
+			mxt_set_sensor_state(data, STATE_WG);
+		else
+#endif
+			mxt_set_sensor_state(data, STATE_SUSPEND);
 		mxt_reset_slots(data);
 
 		if (data->use_regulator)
