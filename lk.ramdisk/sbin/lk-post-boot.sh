a1016459fee0bd6ade4f1dc08130ebc8ec04c3cb
imoseyon <imoseyon@gmail.com>
2014-11-29 00:27:31 -0800

    ramdisk: add init.d and future lkconfig support

---
+++ lk.ramdisk/sbin/lk-post-boot.sh
@@ -0,0 +1,8 @@
+#!/system/bin/sh
+
+# for lkconfig
+[ ! -d "/data/data/leankernel" ] && mkdir /data/data/leankernel
+chmod 755 /data/data/leankernel
+
+# init.d support
+/system/xbin/busybox run-parts /system/etc/init.d
