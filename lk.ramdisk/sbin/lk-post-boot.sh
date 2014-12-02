ba8380fdd4aa4621198ed263bf3a51d6a84343c8
imoseyon <imoseyon@gmail.com>
2014-12-01 18:52:50 -0800

    ramdisk: keep cpu boost enabled during boot - let users disable
    * via lkconfig or something else

--- lk.ramdisk/sbin/lk-post-boot.sh
+++ lk.ramdisk/sbin/lk-post-boot.sh
@@ -7,8 +7,6 @@ chmod 755 /data/data/leankernel
 # init.d support
 /system/xbin/busybox run-parts /system/etc/init.d
 
-echo N > /sys/module/cpu_boost/parameters/cpuboost_enable
-
 #
 # lkconfig settings below
 #
