0ccf74715777c14c823a6c581219522b6465ee4a
imoseyon <imoseyon@gmail.com>
2015-01-16 21:39:04 -0800

    ramdisk: more selinux fixes including CM theme engine fix

--- lk.ramdisk/sbin/lk-post-boot.sh
+++ lk.ramdisk/sbin/lk-post-boot.sh
@@ -8,9 +8,13 @@ chmod 444 /dev/frandom
 /system/xbin/supolicy --live \
 	"allow untrusted_app debugfs file { open read getattr }" \
 	"allow untrusted_app sysfs_lowmemorykiller file { open read getattr }" \
+	"allow untrusted_app persist_file dir { open read getattr }" \
 	"allow debuggerd gpu_device chr_file { open read getattr }" \
 	"allow netd netd capability fsetid" \
-	"allow { system_app shell } dalvikcache_data_file file write"
+	"allow { system_app shell } dalvikcache_data_file file write" \
+	"allow { zygote mediaserver bootanim appdomain }  theme_data_file dir { search r_file_perms r_dir_perms }" \
+	"allow { zygote mediaserver bootanim appdomain }  theme_data_file file { r_file_perms r_dir_perms }" \
+	"allow system_server rootfs dir { open read write getattr }"
 
 # for lkconfig
 [ ! -d "/data/data/leankernel" ] && mkdir /data/data/leankernel
