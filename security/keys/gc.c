965f8c0c4a7eb565886f53ab3c64dc7d071cd50e
imoseyon <imoseyon@gmail.com>
2015-01-29 19:44:53 -0800

    Linux 3.10.67

--- security/keys/gc.c
+++ security/keys/gc.c
@@ -201,7 +201,6 @@ static noinline void key_gc_unused_keys(struct list_head *keys)
 		if (test_bit(KEY_FLAG_INSTANTIATED, &key->flags))
 			atomic_dec(&key->user->nikeys);
 
-
 		/* now throw away the key memory */
 		if (key->type->destroy)
 			key->type->destroy(key);
