231d8f35beee6dc79d278a839e73b1074fac34e7
Gopikrishnaiah Anandan <agopik@codeaurora.org>
2015-02-17 20:59:43 -0800

    msm: mdss: Return ESHUTDOWN error when driver is shutting down
    When MDP driver is being shutdown, driver clients can make ioctl calls.
    Driver was returning a generic error which results in clients
    re-issuing the ioctl. Driver should return ESHUTDOWN error to notify
    the clients that it is shutting down and cannot accept any ioctls.
    Change returns the shutdown error for ioctls issued during shutdown
    sequence.
    
    Change-Id: Ie5a8e557565feb50edd858f9a2ee98637efb55d0
    Signed-off-by: Gopikrishnaiah Anandan <agopik@codeaurora.org>
    
    Conflicts:
    	drivers/video/msm/mdss/mdss_fb.c

--- drivers/video/msm/mdss/mdss_fb.c
+++ drivers/video/msm/mdss/mdss_fb.c
@@ -1687,7 +1687,7 @@ static int mdss_fb_open(struct fb_info *info, int user)
 		pr_err_once("Shutdown pending. Aborting operation. Request from pid:%d name=%s\n",
 			pid, task->comm);
 		sysfs_notify(&mfd->fbi->dev->kobj, NULL, "show_blank_event");
-		return -EPERM;
+		return -ESHUTDOWN;
 	}
 
 	list_for_each_entry(pinfo, &mfd->proc_list, list) {
@@ -2130,7 +2130,7 @@ static int mdss_fb_pan_idle(struct msm_fb_data_type *mfd)
 		mdss_fb_signal_timeline(&mfd->mdp_sync_pt_data);
 	} else if (mfd->shutdown_pending) {
 		pr_debug("Shutdown signalled\n");
-		return -EPERM;
+		return -ESHUTDOWN;
 	}
 
 	return 0;
@@ -2150,7 +2150,7 @@ static int mdss_fb_wait_for_kickoff(struct msm_fb_data_type *mfd)
 
 	} else if (mfd->shutdown_pending) {
 		pr_debug("Shutdown signalled\n");
-		return -EPERM;
+		return -ESHUTDOWN;
 	}
 
 	return 0;
@@ -2883,7 +2883,7 @@ int mdss_fb_do_ioctl(struct fb_info *info, unsigned int cmd,
 		return -EINVAL;
 
 	if (mfd->shutdown_pending)
-		return -EPERM;
+		return -ESHUTDOWN;
 
 	atomic_inc(&mfd->ioctl_ref_cnt);
 
