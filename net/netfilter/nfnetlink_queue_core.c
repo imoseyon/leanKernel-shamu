5ba20d430582b65e11c13c0bc47f2fc7bf8b2327
imoseyon <imoseyon@gmail.com>
2015-04-20 14:59:54 -0700

    Linux 3.10.75

--- net/netfilter/nfnetlink_queue_core.c
+++ net/netfilter/nfnetlink_queue_core.c
@@ -227,8 +227,8 @@ nfqnl_flush(struct nfqnl_instance *queue, nfqnl_cmpfn cmpfn, unsigned long data)
 	spin_unlock_bh(&queue->lock);
 }
 
-static void
-nfqnl_zcopy(struct sk_buff *to, const struct sk_buff *from, int len, int hlen)
+static int
+nfqnl_zcopy(struct sk_buff *to, struct sk_buff *from, int len, int hlen)
 {
 	int i, j = 0;
 	int plen = 0; /* length of skb->head fragment */
