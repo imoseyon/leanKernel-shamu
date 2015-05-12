839abb6ce2e82ef6451f660cb22cc9d54df4784e
imoseyon <imoseyon@gmail.com>
2015-05-12 12:58:48 -0700

    ramdisk: lkconfig changes to accommodate new custom_max_freq param

--- lk.ramdisk/sbin/lk-post-boot.sh
+++ lk.ramdisk/sbin/lk-post-boot.sh
@@ -142,6 +142,7 @@ fi
 CFILE="/data/data/leankernel/maxfreq"
 if [ -f $CFILE ]; then 
 	FREQ=`cat $CFILE`
+	echo $FREQ > /sys/module/cpufreq/parameters/custom_max_freq
 	echo $FREQ > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
 	echo $FREQ > /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq
 	echo $FREQ > /sys/devices/system/cpu/cpu2/cpufreq/scaling_max_freq
