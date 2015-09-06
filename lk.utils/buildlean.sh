205c3a233b0949bd9358cbf2a3b87dd985e53956
imoseyon <imoseyon@gmail.com>
2015-09-06 08:45:53 -0700

    lk.utils: grab latest build stuff from lk-lp branch

--- lk.utils/buildlean.sh
+++ lk.utils/buildlean.sh
@@ -6,6 +6,8 @@ objdir="/lk2/shamu_obj"
 device="shamu"
 cc="/data/linaro/49/arm-cortex_a15-linux-gnueabihf-linaro/bin/arm-eabi-"
 filename="lk_${device}_lp-v${1}.zip"
+ocuc_branch="lk-lp-ocuc"
+mkbootimg="/data/mkbootimg_tools2/mkbootimg"
 
 compile() {
   cd $sdir
@@ -15,14 +17,14 @@ compile() {
 
 ramdisk() {
   cd $sdir/lk.ramdisk
-  chmod 750 init* charger sbin/adbd
-  chmod 644 default* uevent* res/images/charger/* lpm.rc
-  chmod 755 sbin
+  chmod 750 init* sbin/adbd* sbin/healthd
+  chmod 644 default* uevent* res/images/charger/*
+  chmod 755 sbin sbin/lkconfig
   chmod 700 sbin/lk-post-boot.sh
   chmod 755 res res/images res/images/charger
   chmod 640 fstab.shamu
   find . | cpio -o -H newc | gzip > /tmp/ramdisk.img
-  /data/mkbootimg_tools/mkbootimg_dtb --kernel $objdir/arch/arm/boot/zImage-dtb  --ramdisk /tmp/ramdisk.img --cmdline "console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=shamu msm_rtb.filter=0x37 ehci-hcd.park=3 utags.blkdev=/dev/block/platform/msm_sdcc.1/by-name/utags utags.backup=/dev/block/platform/msm_sdcc.1/by-name/utagsBackup coherent_pool=8M" --pagesize 2048 -o /tmp/boot.img
+  $mkbootimg --kernel $objdir/arch/arm/boot/zImage-dtb  --ramdisk /tmp/ramdisk.img --cmdline "console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=shamu msm_rtb.filter=0x37 ehci-hcd.park=3 utags.blkdev=/dev/block/platform/msm_sdcc.1/by-name/utags utags.backup=/dev/block/platform/msm_sdcc.1/by-name/utagsBackup coherent_pool=8M" --pagesize 2048 -o /tmp/boot.img
 }
 
 zipit() {
@@ -34,5 +36,14 @@ zipit() {
   cd $sdir
 } 
 
+cm_ramdisk() {
+  cd /tmp/cm/ramdisk
+  cp init sepolicy init.cm.rc init.environ.rc init.superuser.rc *_contexts $sdir/lk.ramdisk
+  cd $sdir
+}
+
+[[ $1 =~ "cm" ]] && cm_ramdisk && sed -i '/SYSTEMSERVERCLASSPATH \/system\/framework\/services/d' lk.ramdisk/init.lk.rc
+[[ $1 =~ "ocuc" ]] && git checkout $ocuc_branch arch/arm/boot/dts/qcom/apq8084.dtsi drivers/thermal/lk_thermal.h
 compile $1 && ramdisk && zipit $filename
-#compile $1 && ramdisk 
+[[ $1 =~ "ocuc" ]] && git checkout HEAD arch/arm/boot/dts/qcom/apq8084.dtsi drivers/thermal/lk_thermal.h
+[[ $1 =~ "cm" ]] && git checkout HEAD lk.ramdisk
